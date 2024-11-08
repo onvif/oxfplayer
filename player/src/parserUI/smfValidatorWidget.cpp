af49144c6ecb2056579118674b626ba1528b38b3
Andrew Guz <116361992+andrewGuzGRSE@users.noreply.github.com>
2024-11-08 19:51:40 +0300

    Validation dialog updated

--- player/src/parserUI/smfValidatorWidget.cpp
+++ player/src/parserUI/smfValidatorWidget.cpp
@@ -7,9 +7,7 @@ SMFValidationWidget* widget = nullptr;
 
 void validationFinished(ValidationResult validationResult) {
     if (widget)
-        widget->accept();
-    widget->validationCallback(validationResult);
-    //validation_result_free();
+        widget->validationCallback(validationResult);
 }
 
 SMFValidationWidget::SMFValidationWidget(QWidget* parent, const QString& file_name, const QString& codecString)
@@ -38,19 +36,6 @@ SMFValidationWidget::SMFValidationWidget(QWidget* parent, const QString& file_na
         widget = nullptr;
         this->accept();
     });
-    
-    // uncomment this to see how it will looks like
-    //ValidationResult validation_result;
-    //validation_result.public_key_is_valid = true;
-    //validation_result.video_is_valid = true;
-    //validation_result.vendor_info.serial_number = "N/A";
-    //validation_result.vendor_info.firmware_version = "v0.0.4";
-    //validation_result.vendor_info.manufacturer = "Signed Media Framework";
-    //validation_result.gop_info.valid_gops_count = 50;
-    //validation_result.gop_info.valid_gops_with_missing_nalu_count = 0;
-    //validation_result.gop_info.invalid_gops_count = 0;
-    //validation_result.gop_info.gops_without_signature_count = 0;
-    //validationCallback(validation_result);
 }
 
 SMFValidationWidget::~SMFValidationWidget() {
@@ -63,23 +48,38 @@ void SMFValidationWidget::validationCallback(ValidationResult validationResult) 
 
     QString message;
     message += QString{ "PUBLIC KEY IS " } + (validationResult.public_key_is_valid ? QString{ "VALID" } : QString{ "INVALID" }) + QString{ "<br/>" };
-    message += QString{ "VIDEO IS " } + (validationResult.public_key_is_valid ? QString{ "VALID" } : QString{ "INVALID" }) + QString{ "<br/>" };
+    message += QString{ "VIDEO IS " } + (validationResult.video_is_valid ? QString{ "VALID" } : QString{ "INVALID" }) + QString{ "<br/>" };
     message += QString{ "<br/>" };
-    message += QString{ "Vendor Info:<br/>" };
-    message += QString{ "Serial Number: " } +
-               (validationResult.vendor_info.serial_number ? QString{ validationResult.vendor_info.serial_number } : QString{ "N/A" }) +
-               QString{ "<br/>" };
-    message += QString{ "Firmware Version: " } +
-               (validationResult.vendor_info.firmware_version ? QString{ validationResult.vendor_info.firmware_version } : QString{ "N/A" }) +
-               QString{ "<br/>" };
-    message += QString{ "Manufacturer: " } +
-               (validationResult.vendor_info.manufacturer ? QString{ validationResult.vendor_info.manufacturer } : QString{ "N/A" }) +
-               QString{ "<br/>" };
-    message += QString{ "<br/>" };
-    message += QString{ "GOP Info:<br/>" };
-    message += QString{ "Number of valid GOPs: " } + QString::number(validationResult.gop_info.valid_gops_count) + QString{ "<br/>" };
-    message += QString{ "Number of valid GOPs with missing NALUs: " } + QString::number(validationResult.gop_info.valid_gops_with_missing_nalu_count) + QString{ "<br/>" };
-    message += QString{ "Number of invalid GOPs: " } + QString::number(validationResult.gop_info.invalid_gops_count) + QString{ "<br/>" };
-    message += QString{ "Number of GOPs without signature: " } + QString::number(validationResult.gop_info.gops_without_signature_count) + QString{ "<br/>" };
+    if (!validationResult.public_key_is_valid ||
+        !validationResult.video_is_valid) {
+        if (strlen(validationResult.video_error_str) != 0) {
+            message += QString{ "Video error: %1<br/>" }.arg(validationResult.video_error_str);
+        }
+        if (strlen(validationResult.key_validation_str) != 0) {
+            message += QString{ "Key validation error: %1<br/>" }.arg(validationResult.key_validation_str);
+        }
+        if (strlen(validationResult.video_valid_str) != 0) {
+            message += QString{ "Video validation info: %1<br/>" }.arg(validationResult.video_valid_str);
+        }
+    } else {
+        message += QString{ "Vendor Info:<br/>" };
+        message += QString{ "Serial Number: " } +
+                   (validationResult.vendor_info.serial_number ? QString{ validationResult.vendor_info.serial_number } : QString{ "N/A" }) +
+                   QString{ "<br/>" };
+        message += QString{ "Firmware Version: " } +
+                   (validationResult.vendor_info.firmware_version ? QString{ validationResult.vendor_info.firmware_version } : QString{ "N/A" }) +
+                   QString{ "<br/>" };
+        message += QString{ "Manufacturer: " } +
+                   (validationResult.vendor_info.manufacturer ? QString{ validationResult.vendor_info.manufacturer } : QString{ "N/A" }) +
+                   QString{ "<br/>" };
+        message += QString{ "<br/>" };
+        message += QString{ "GOP Info:<br/>" };
+        message += QString{ "Number of valid GOPs: " } + QString::number(validationResult.gop_info.valid_gops_count) + QString{ "<br/>" };
+        message += QString{ "Number of valid GOPs with missing NALUs: " } +
+                   QString::number(validationResult.gop_info.valid_gops_with_missing_nalu_count) + QString{ "<br/>" };
+        message += QString{ "Number of invalid GOPs: " } + QString::number(validationResult.gop_info.invalid_gops_count) + QString{ "<br/>" };
+        message += QString{ "Number of GOPs without signature: " } + QString::number(validationResult.gop_info.gops_without_signature_count) +
+                   QString{ "<br/>" };
+    }
     m_ui->textEdit->setText(message);
 }
